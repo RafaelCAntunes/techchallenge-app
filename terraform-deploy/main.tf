3c7f02ddc1701d213b24492fe9ed3c3f411cc678
Rafael Antunes <rafaelcantunes3@gmail.com>
2025-10-07 23:03:38 -0300

    adicionando config das credenciais do aws

--- terraform-deploy/main.tf
+++ terraform-deploy/main.tf
@@ -48,9 +48,16 @@ data "terraform_remote_state" "db" {
   }
 }
 
-provider "kubernetes" {
-  host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
-  cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.eks_cluster_ca_certificate)
-  token                  = data.terraform_remote_state.eks.outputs.eks_cluster_auth_token
+data "aws_eks_cluster" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_name
 }
 
+data "aws_eks_cluster_auth" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
